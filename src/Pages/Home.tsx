c868dd301052a38fa3ebc053ddda4fa747fd30a4
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 04:05:47 +0530

    create Link for go to the Pages

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TopicBox from '../components/TopicBox'
 import Content from '../components/Content'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
         <h1>Home Page</h1>
+        <Link to="/About"> Go to About the pages</Link>
 
     </div>
   )
