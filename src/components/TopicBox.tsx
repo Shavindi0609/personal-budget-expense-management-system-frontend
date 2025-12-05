4c9ded4f90a54514f79e1819ced9e0cba9934450
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-05 20:10:25 +0530

    Dinamically Value add step with variables

--- src/components/TopicBox.tsx
+++ src/components/TopicBox.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+
+  const topic = "My name is Shavindi"
+  const item = "Ridmamli Aloka"
+
   return (
     <div>
         <div className="topicBox">
-            <span className="text">This is the Topic</span>
+            <span className="text">{topic} {item}</span>
         </div>
     </div>
   )
