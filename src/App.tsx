184e29a7f15ec6bc6f19f6e7fbd21a934831bcda
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-05 17:47:34 +0530

    create page as a component & call it App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import First from './components/First'
+import Home from './Pages/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <h1>This is my first react project</h1>
-      <First/>
+     <Home/>
     </div>
   
   )
