c9db9718922cb69db5170be55088824002728bef
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 19:03:12 +0530

    Create Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Pages/Home'
 import Accordion from 'react-bootstrap/Accordion';
 import React, {useState, useEffect} from 'react';
 
--- src/Pages/Conact.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Conact() {
-  return (
-    <div>
-        <h1>This is Contact Page</h1>
-    </div>
-  )
-}
