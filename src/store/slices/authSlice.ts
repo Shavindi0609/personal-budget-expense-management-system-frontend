ec94d8e1ebc214221b519117274d0a4f7612ae32
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 19:17:08 +0530

    Create Auth Slice for token store

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+  accessToken: string | null;
+}
+
+const initialState: AuthState = {
+  accessToken: localStorage.getItem("accessToken") || null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setToken: (state, action: PayloadAction<string>) => {
+      state.accessToken = action.payload;
+      localStorage.setItem("accessToken", action.payload);
+    },
+    logout: (state) => {
+      state.accessToken = null;
+      localStorage.removeItem("accessToken");
+    },
+  },
+});
+
+export const { setToken, logout } = authSlice.actions;
+export default authSlice.reducer;
