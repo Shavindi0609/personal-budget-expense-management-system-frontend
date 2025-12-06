7108886871282ec0bf8191a448ce6e485340be2e
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 18:23:32 +0530

    use Effect (side Effect)

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
 import Accordion from 'react-bootstrap/Accordion';
+import React, {useState, useEffect} from 'react';
 
-function buttonClick() {
-  console.log("Button is clicked")
-  alert("Button is clicked");
-}
+
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+   const decrement = () => {
+    setCount(count - 1);
+  }
+
+  useEffect(() => {
+    console.log(`new count is: ${count}`);
+ 
+  },[count]
+
+  )
+
+
   return (
    <div>
-      <h1>React Event Handling</h1>
-      <br></br><hr></hr>
-      <button onClick={buttonClick}>Click Here</button>
+    <h1>Count: {count}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+      
    </div>
   );
 }
