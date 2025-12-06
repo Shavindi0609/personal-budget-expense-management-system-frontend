caf570208109631895a554bac0cfb17bf3a53f06
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 19:51:32 +0530

    Create Protected Route for security

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../store/hooks";
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const token = useAppSelector((s) => s.auth.accessToken);
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
+
+
+//secure (login,ckeck token)
