78fabd0b63d1cd2b5790bab3d79650dcefcdde52
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-06 18:38:09 +0530

    setup Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,11 @@ import React, {useState, useEffect} from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  }
-
-   const decrement = () => {
-    setCount(count - 1);
-  }
-
-  useEffect(() => {
-    console.log(`new count is: ${count}`);
  
-  },[count]
-
-  )
-
 
   return (
    <div>
-    <h1>Count: {count}</h1>
-    <button onClick={increment}>+</button>
-    <button onClick={decrement}>-</button>
-      
+      <h1>Finwise</h1>
    </div>
   );
 }
