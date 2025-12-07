b28f78b5db5009e42fe2c698132aafafd494ab3b
#Shanu0609@ <shavindialoka69@gmail.com>
2025-12-07 13:19:04 +0530

    Frontend Backend Connect & Register, Login Successfull

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../store/hooks";
+import type { JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const token = useAppSelector((s) => s.auth.accessToken);
