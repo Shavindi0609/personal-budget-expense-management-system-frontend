896c51cd88d428aaf167f19d3d278aefd616dea6
#Shanu0609@ <shavindialoka69@gmail.com>
2026-01-05 15:53:01 +0530

    Login First

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,10 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-gray-50">
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<HomePublic />} />
-        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/" element={<HomePublic />} /> */}
+                <Route path="/" element={<LoginPage />} />
+        
+        {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="/register" element={<RegisterPage />} />
 
         {/* Admin Dashboard */}
