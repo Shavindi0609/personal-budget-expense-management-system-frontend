a61a2e480750d5ef1e7d72f76a463a729e73af2b
#Shanu0609@ <shavindialoka69@gmail.com>
2026-01-08 00:58:55 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // මෙම පේළිය අනිවාර්යයෙන් එක් කරන්න
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), // මේක අනිවාර්යයෙන්ම තියෙන්න ඕනේ
+    react(), // මෙහිදී react() ශ්‍රිතය අනිවාර්යයෙන් අඩංගු විය යුතුය
     tailwindcss(),
   ],
-  base: '/', // paths නිවැරදිව වැඩ කිරීමට මෙය එක් කරන්න
+  base: '/', // paths නිවැරදිව හඳුනා ගැනීමට මෙය වැදගත් වේ
 })
