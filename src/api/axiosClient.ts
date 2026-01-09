45043898dc798cf122dbfdbefcec3b668ca3f0b4
#Shanu0609@ <shavindialoka69@gmail.com>
2026-01-09 22:56:03 +0530

    deploy vercel

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,30 +1,30 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || "https://personal-budget-expense-management.onrender.com/api/v1",
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://personal-budget-expense-management.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "https://personal-budget-expense-management.onrender.com/api/v1",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://personal-budget-expense-management.onrender.com/api/v1",
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("accessToken");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
 
